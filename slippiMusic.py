f422ba892b97b775fff3ac426f03c282a1cb4274
Braulio Carrion Corveira <45177553+Carr-23@users.noreply.github.com>
2020-07-01 13:41:32 -0400

    Added proper search that isn't blocked by youtube and created folders

--- slippiMusic.py
+++ slippiMusic.py
@@ -1,14 +1,11 @@
 import getpass
-#import numpy as np
 import cv2
 import keyboard
 import pyautogui
+from fast_youtube_search import search_youtube
+import os
 import pytesseract
-
-import requests
-import urllib
-from bs4 import BeautifulSoup
-
+import pafy
 
 def imageChanges():
     # Takes a screenshot of the current screen and saves it
@@ -26,38 +23,71 @@ def imageChanges():
     crop = img[940:1080, 710:1210]
     cv2.imwrite(screenshotPath, crop)
 
+    # Pass the final image to the OCR function to check it over
     finalImage = cv2.imread(screenshotPath)
     ocr(finalImage)
+
 
 def ocr(img):
     #
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     #
 
+    # OCR and passes the text to the stageMusic
     config = ('-l eng --oem 1 --psm 3')
     stage = pytesseract.image_to_string(img, config=config)
-    stageSong(stage)
+    #stageMusic(stage)
 
+def ytSearch():
+    results = search_youtube('idk'.split())
+    print('https://www.youtube.com/watch?v=' + results[0]['id'])
 
+def startUpCheck():
+    try:
+        os.mkdir(os.path.join('C:\\Users\\' + checkuser + '\\Documents', 'Rollback Music ALT'))
+    except OSError as error:
+        print()
+
+    generalPath = 'C:\\Users\\' + checkuser + '\\Documents\\Rollback Music ALT'
+
+    path = [
+            os.path.join(generalPath, 'Dream Land N64 - Music'),
+            os.path.join(generalPath, 'Fountain of Dream - Music'),
+            os.path.join(generalPath, 'Pokémon Stadium - Music'),
+            os.path.join(generalPath, 'Yoshi\'s Story - Music'),
+            os.path.join(generalPath, 'Final Destination - Music'),
+            os.path.join(generalPath, 'Battlefield - Music'),
+            os.path.join(generalPath, '0Temp')
+            ]
+
+    for x in range(7):
+        try:
+            os.mkdir(path[x])
+        except OSError as error:
+            print()
+
+    defaultIDS = [
+                  '40SjNtbuNYU',
+                  'pz3BQFXjEOI',
+                  'mS8-zZ9EZCA',
+                  'EcHiSeA57TU',
+                  'L8EfpcF9rRc',
+                  'u72SLGULM8A'
+                 ]
+    for x in range(6):
+        pafy.new('https://www.youtube.com/watch?v=' + defaultIDS[x]).getbestaudio().download(generalPath + path[x])
 
 if __name__ == "__main__":
 
-    #Check name of user in order to choose correct path, since each computer has a different user
+    # Check name of user in order to choose correct path, since each computer has a different user
     checkuser = getpass.getuser()
 
-    #Add the username to the path
-    screenshotPath = 'C:\\Users\\' + checkuser + '\\Documents\\stageSS.png'
+    # Add the username to the path
+    screenshotPath = 'C:\\Users\\' + checkuser + '\\Documents\\Rollback Music ALT\\0Temp\\stageSS.png'
+
+    startUpCheck()
 
     # Waits for space to be hit, SHOULD happen after every game
-    #keyboard.wait('space')
-
-    textToSearch = 'test'
-    query = urllib.parse.quote(textToSearch)
-    url = "https://www.youtube.com/results?search_query=" + query
-
-    response = requests.get(url)
-    data = response.text
-    soup = BeautifulSoup(data, 'lxml')
-
-    for vid in soup.findAll(attrs={'class': 'yt-uix-tile-link'}):
-        print('https://www.youtube.com' + vid['href'])
+    keyboard.wait('space')
+    imageChanges()
+    print("test")
